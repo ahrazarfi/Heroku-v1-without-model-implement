252c4eeebed1970615c99b7564f35802d5b6369b
ahrazarfi <ahrazarfi@gmail.com>
2021-05-27 22:22:03 +0530

    CHORE: formatted and refactored

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import streamlit as st
 DISEASES = {
     "Diabetic Retinopathy (Upload your retinal fundus images)": "dia",
     "Malaria (Upload cell image)": "mal",
-    "Heart Checkup (Upload Cardiac image)": "oct"
+    "Heart Checkup (Upload Cardiac image)": "oct",
 }
 
 
@@ -15,9 +15,9 @@ st.title("DISEASE PREDICTOR")
 
 disease = st.selectbox("Choose the disease", [i for i in DISEASES.keys()])
 
-if(disease == "Diabetic Retinopathy (Upload your retinal fundus images)"):
+if disease == "Diabetic Retinopathy (Upload your retinal fundus images)":
     disease_URL = "dia"
-elif(disease == "Malaria (Upload cell image)"):
+elif disease == "Malaria (Upload cell image)":
     disease_URL = "mal"
 else:
     disease_URL = "oct"
@@ -26,17 +26,21 @@ image = st.file_uploader("Choose an image")
 
 
 def disease_prediction_Handler(result):
-    rem_list = ['empty', 'type'] # source: "https://www.kite.com/python/answers/how-to-remove-multiple-keys-from-a-dictionary-in-python"
-    if(disease_URL=="dia"):
+    rem_list = [
+        "empty",
+        "type",
+    ]  # source: "https://www.kite.com/python/answers/how-to-remove-multiple-keys-from-a-dictionary-in-python"
+    if disease_URL == "dia":
         for key in rem_list:
             del result[key]
-    elif(disease_URL=="dia"):
+    elif disease_URL == "dia":
         for key in rem_list:
             del result[key]
     else:
         for key in rem_list:
             del result[key]
     return result
+
 
 if st.button("Analyze"):
     if image is not None and disease_URL is not None:
@@ -45,7 +49,9 @@ if st.button("Analyze"):
 
         # result = res.text # this converts the response into plain text
 
-        result = res.json() # returns the response from the API in JSON format, returns a dictionary
+        result = (
+            res.json()
+        )  # returns the response from the API in JSON format, returns a dictionary
 
         # result = json.loads(result) # while using got error "the JSON object must be str, bytes or bytearray, not dict",  ref: "https://stackoverflow.com/questions/42354001/python-json-object-must-be-str-bytes-or-bytearray-not-dict"
 
@@ -56,5 +62,3 @@ if st.button("Analyze"):
             st.write(value)
         # result = result['pred_val']
         # st.write(result)
-
-
